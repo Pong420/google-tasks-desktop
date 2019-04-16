137a4e18bb571b42e3d4d4bc48406e8c03ee54d7
Pong <samfunghp@gmail.com>
2019-04-16 09:43:13 +0800

    add auth page

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,21 +1,48 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { RootState, AuthActionCreators, AuthsState } from '../../store';
+import { Input } from '../Mui/Input';
+import { useInput } from '../../utils/useInput';
+import { ReactComponent as LogoSvg } from '../../assets/logo.svg';
+import Button from '@material-ui/core/Button';
 
 const mapStateToProps = ({ auth }: RootState) => ({ ...auth });
 const mapDispatchToProps = (dispatch: Dispatch) =>
   bindActionCreators(AuthActionCreators, dispatch);
 
 export function AuthComponent({
+  loggedIn,
   autoLogin,
-  authenticate
+  authenticate,
+  getToken
 }: AuthsState & typeof AuthActionCreators) {
+  const code = useInput('');
+
   useEffect(() => {
     autoLogin && authenticate();
   }, [authenticate, autoLogin]);
 
-  return null;
+  if (autoLogin || loggedIn) {
+    return null;
+  }
+
+  return (
+    <div className="auth">
+      <LogoSvg className="logo" />
+      <h4>Unoffical Google Task Clinet</h4>
+      <form action="">
+        Paste the code here:
+        <Input {...code} autoFocus fullWidth className="filled bottom-border" />
+        <div>
+          <Button onClick={authenticate}>Get Code</Button>
+          <Button color="secondary" onClick={() => getToken(code.value)}>
+            Confirm
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export const Auth = connect(
