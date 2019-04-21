9ad678e0097a6288bcef324cad4f4c142cf6ba71
Pong <samfunghp@gmail.com>
2019-04-21 14:28:32 +0800

    update app region hanlding

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Auth } from './components/Auth';
+import { AppRegion } from './components/AppRegion';
 import { TaskList } from './components/TaskList';
 import { PATHS } from './constants';
 
 const App = () => (
   <>
     <Auth />
+    <AppRegion />
     <Switch>
       <Route exact path={PATHS.TASKLIST} component={TaskList} />
       <Redirect to={PATHS.TASKLIST} />
