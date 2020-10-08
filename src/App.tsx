1e48be2c68d1e8c4acbbdbef1c5e5170ab51ca81
Pong <samfunghp@gmail.com>
2020-10-08 23:25:27 +0800

    fix: app region for macos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import { Route, Switch, Redirect, generatePath } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { AppRegion } from './components/AppRegion';
 import { PrivateRoute } from './components/PrivateRoute';
 import { Auth } from './pages/Auth';
 import { TaskList } from './pages/TaskList';
 import { PATHS } from './constants';
-import { titleBarSelector } from './store';
 
 const App = () => {
-  const titleBar = useSelector(titleBarSelector);
-
   return (
     <>
-      {titleBar === 'native' ? null : <AppRegion />}
-      <>
-        <Switch>
-          <Route path={PATHS.AUTH} component={Auth} />
-          <PrivateRoute path={PATHS.TASKLIST} component={TaskList} />
-          <Redirect to={generatePath(PATHS.TASKLIST, {})} />
-        </Switch>
-      </>
+      <AppRegion />
+      <Switch>
+        <Route path={PATHS.AUTH} component={Auth} />
+        <PrivateRoute path={PATHS.TASKLIST} component={TaskList} />
+        <Redirect to={generatePath(PATHS.TASKLIST, {})} />
+      </Switch>
     </>
   );
 };
--- src/components/AppRegion/AppRegion.tsx
+++ src/components/AppRegion/AppRegion.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { IconButton } from '../Mui';
+import { titleBarSelector } from '../../store';
 import Close from '@material-ui/icons/Close';
 
 const { close } = window.getCurrentWindow();
 
 export function AppRegion() {
+  const titleBar = useSelector(titleBarSelector);
+
   return (
     <div className="app-region">
-      <div className="simple-title-bar">
-        {/* should not pass `close` function directly into `onClick` props */}
-        <IconButton icon={Close} onClick={() => close()} />
-      </div>
+      {window.platform === 'darwin' ? (
+        <div className="app-region-drag" />
+      ) : titleBar === 'simple' ? (
+        <div className="simple-title-bar">
+          {/* should not pass `close` function directly into `onClick` props */}
+          <IconButton icon={Close} onClick={() => close()} />
+        </div>
+      ) : null}
     </div>
   );
 }
