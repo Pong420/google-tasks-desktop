3c967ec51a23e4d96de22c8ca94d580ea44346ab
Pong <samfunghp@gmail.com>
2020-11-28 16:06:07 +0800

    fix: development

--- scripts/electron-wait-react.js
+++ scripts/electron-wait-react.js
@@ -1,20 +1,27 @@
-// @ts-check
 const exec = require('child_process').exec;
 const net = require('net');
 
-const client = new net.Socket();
-const port = process.env.PORT ? Number(process.env.PORT) - 100 : 3000;
+let client = new net.Socket();
+const port = process.env.PORT ? process.env.PORT - 100 : 3000;
 
 process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 
+let startedElectron = false;
 const tryConnection = () =>
   client.connect({ port: port }, () => {
     client.end();
-    process.exit(0);
+    if (!startedElectron) {
+      client.destroy();
+      console.log('starting electron');
+      startedElectron = true;
+      exec('npm run electron:dev');
+    }
   });
 
 exec('npm run electron:compile', tryConnection);
 
 client.on('error', error => {
+  client.destroy();
+  client = new net.Socket();
   setTimeout(tryConnection, 1000);
 });
